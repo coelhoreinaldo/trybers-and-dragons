779ea29de220df9ee4d890f36537bc2ae96269e5
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-22 14:10:24 -0300

    ✨ feat: req 1, create Race class

---
+++ src/Races/Race.ts
@@ -0,0 +1,17 @@
+export default abstract class Race {
+  constructor(protected _name:string, protected _dexterity: number) {}
+
+  get name():string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+  
+  static createdRacesInstances():number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints():number;
+}
