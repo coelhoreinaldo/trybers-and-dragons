9318585873a1aa363ef09dcab898f41a9e0cae32
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-22 17:49:10 -0300

    ✨ feat: req 13, start game

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import Character from './Character';
+import Monster from './Monster';
+import Dragon from './Dragon';
+import Battle, { PVE, PVP } from './Battle';
+
+const player1 = new Character('player1');
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+const player2 = new Character('player2');
+const player3 = new Character('player3');
+
+// log(player1);
+// log(player2);
+// log(player3);
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+// log(monster1);
+// log(monster2);
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+const runBattles = (battles: Battle[]) => {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+};
+
+export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
